a2a04b46ca9a993523fbb7a6886bc4cebe7c3229
Florian Heller <florian.heller@uhasselt.be>
2018-11-14 15:48:12 +0100

    Initial Commit

---
+++ Swift Headset X/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Headset X
+//
+//  Created by Florian Heller on 14.11.18.
+//  Copyright © 2018 Florian Heller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
